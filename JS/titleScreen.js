c4e5ef21179a787ff2e91a18511dcf18f351613c
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-06-01 16:56:39 -0700

    Create titleScreen.js

---
+++ JS/titleScreen.js
@@ -0,0 +1,12 @@
+let savedInfo = {};
+
+continueGameBtn.addEventListener('click', function() {
+if(localStorage.getItem('playerInfo') != null) { 
+    savedInfo = JSON.parse(localStorage.getItem('playerInfo'));
+    if(savedInfo.gameLevel === 1) {
+        location.href = './landingSite.html';
+    }
+} else {
+    location.href = './index.html';
+}
+});
