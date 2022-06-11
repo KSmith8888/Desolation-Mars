ad5f7c7d15592e2ea98d1a7676720ed4c118482e
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-06-11 12:48:35 -0700

    Update titleScreen.js

--- JS/titleScreen.js
+++ JS/titleScreen.js
@@ -1,4 +1,34 @@
+const startGameBtn = document.getElementById('startGameBtn');
+const continueGameBtn = document.getElementById('continueGameBtn');
+const optionsBtn = document.getElementById('optionsBtn');
 let savedInfo = {};
+
+//Start new game button sends user to level 1
+startGameBtn.addEventListener('keydown', function(e) {
+    if(e.key == 'enter') {
+        location.href = './landingSite.html';
+    }
+});
+
+startGameBtn.addEventListener('click', function() {
+    location.href = './landingSite.html';
+});
+
+/*
+Continue game button accesses saved player info if it is present in local storage, sets player object values to match saved info and sends user to correct level
+*/
+continueGameBtn.addEventListener('keydown', function(e) {
+    if(e.key == 'enter') {
+        if(localStorage.getItem('playerInfo') != null) { 
+            savedInfo = JSON.parse(localStorage.getItem('playerInfo'));
+            if(savedInfo.gameLevel === 1) {
+                location.href = './landingSite.html';
+            }
+        } else {
+            alert('No save data found, please start a new game.');
+        }
+    }
+});
 
 continueGameBtn.addEventListener('click', function() {
 if(localStorage.getItem('playerInfo') != null) { 
@@ -7,6 +37,6 @@ if(localStorage.getItem('playerInfo') != null) {
         location.href = './landingSite.html';
     }
 } else {
-    location.href = './index.html';
+    alert('No save data found, please start a new game.');
 }
 });
