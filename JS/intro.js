db6417c29c351c2d3897ae03614511b6b8ee100d
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-06-13 17:26:26 -0700

    Create intro.js

---
+++ JS/intro.js
@@ -0,0 +1,18 @@
+const introContainer = document.getElementById('introContainer');
+const arrowRight = document.getElementById('arrowRight');
+const transmissionContainer = document.getElementById('transmissionContainer');
+const arrowRight2 = document.getElementById('arrowRight2');
+
+transmissionContainer.style.display = 'none';
+arrowRight2.style.display = 'none';
+
+arrowRight.addEventListener('click', function() {
+    introContainer.style.display = 'none';
+    arrowRight.style.display = 'none';
+    transmissionContainer.style.display = 'contents';
+    arrowRight2.style.display = 'grid';
+});
+
+arrowRight2.addEventListener('click', function() {
+    location.href = './landingSite.html';
+});
