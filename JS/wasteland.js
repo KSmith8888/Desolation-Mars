46440075cf33a0b5016d0fd3f2411a8887f5c10f
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-07-08 17:47:06 -0700

    Update wasteland.js

--- JS/wasteland.js
+++ JS/wasteland.js
@@ -3,12 +3,14 @@ const arrowRight2 = document.getElementById('arrowRight2');
 arrowRight2.style.display = 'none';
 let text1 = document.getElementById('text1');
 let text2 = document.getElementById('text2');
+arrowRight1.focus();
 
 arrowRight1.addEventListener('click', function() {
     text2.style.display = 'none';
     arrowRight1.style.display = 'none';
     text1.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum at suscipit velit culpa est corrupti perferendis deleniti officiis eum ipsa, doloremque optio maiores perspiciatis beatae eaque sed eveniet, tenetur iure sint! Harum facere autem hic quidem molestias beatae possimus dolores quisquam eos, nostrum voluptas porro tenetur, accusantium earum reiciendis vel accusamus veritatis odio est praesentium ullam sint ut temporibus consectetur. Neque tenetur praesentium at sed minus, porro eligendi! Voluptatem atque ex, id qui corporis inventore.';
     arrowRight2.style.display = 'grid';
+    arrowRight2.focus();
 });
 
 arrowRight2.addEventListener('click', function() {
