dd525f0ae0f0e6038b01a45d17278dcb8df20d70
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-07-17 17:34:46 -0700

    Update titleScreen.js
    Added sound effect when buttons are clicked.

--- JS/titleScreen.js
+++ JS/titleScreen.js
@@ -3,11 +3,14 @@ const continueGameBtn = document.getElementById('continueGameBtn');
 const gameInfoBtn = document.getElementById('gameInfoBtn');
 let savedInfo = {};
 const titleScreenAudio = document.getElementById('titleScreenAudio');
+menuSound = new Audio();
+menuSound.src = 'Audio/beep.wav';
 
 /*
 Start new game button sends user to level 1. If user has a save file already, prompts if they want to delete it to start a new game.
 */
 startGameBtn.addEventListener('click', function() {
+    menuSound.play();
     if(localStorage.getItem('playerInfo') != null) { 
         if(window.confirm('You have save data for this game, if you start a new game your previous save file will be deleted. Are you sure you want to delete your save file and start a new game?')) {
         localStorage.clear();
@@ -22,12 +25,15 @@ startGameBtn.addEventListener('click', function() {
 Continue game button accesses saved player info if it is present in local storage, sets player object values to match saved info and sends user to correct level
 */
 continueGameBtn.addEventListener('click', function() {
+    menuSound.play();
 if(localStorage.getItem('playerInfo') != null) { 
     savedInfo = JSON.parse(localStorage.getItem('playerInfo'));
     if(savedInfo.gameLevel === 1) {
         location.href = './landingSite.html';
     } else if(savedInfo.gameLevel === 2) {
         location.href = './colonyDelta.html';
+    } else if(savedInfo.gameLevel === 3) {
+        alert('The level you have reached is not yet available. Please start a new game or come back when the game is finished to continue.');
     }
 } else {
     alert('No save data found, please start a new game.');
@@ -40,4 +46,8 @@ document.addEventListener('mousemove', function() {
 
 document.addEventListener('keydown', function() {
     titleScreenAudio.play();
+});
+
+gameInfoBtn.addEventListener('click', function() {
+    menuSound.play();
 });
